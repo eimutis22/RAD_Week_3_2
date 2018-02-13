6efe56c2babe6e9500a57bad6843f47613bec5bd
Eimutisg <eimutis.genys@gmail.com>
2018-02-13 19:21:05 +0000

    CustomerRepository

---
+++ Week_3_2/Week_3_2/DAL/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_3_2.Models;
+
+namespace Week_3_2.DAL
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private SampleDBEntities context;
+
+        public CustomerRepository(SampleDBEntities context)
+        {
+            this.context = context;
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public Customer GetCustomerByID(int id)
+        {
+            return context.Customers.Find(id);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
---
+++ Week_3_2/Week_3_2/DAL/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_3_2.Models;
+
+namespace Week_3_2.DAL
+{
+    interface ICustomerRepository : IDisposable
+    {
+        List<Customer> GetCustomers();
+        Customer GetCustomerByID(int id);
+    }
+}
